b50142ada18d2fa22749845f42fc337a1300a3a9
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-22 18:21:53 +0100

    display PlaceItem

--- screens/AllPlaces.jsx
+++ screens/AllPlaces.jsx
@@ -1,12 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import PlacesList from '../components/Places/PlacesList'
 
 const AllPlaces = () => {
-  return (
-    <View>
-      <Text>AllPlaces</Text>
-    </View>
-  )
+  return <PlacesList />
 }
 
 export default AllPlaces
