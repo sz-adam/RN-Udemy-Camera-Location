31a037ec061f040133bd91ae3cf7fda875a317f9
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-22 18:21:22 +0100

    help place

---
+++ models/Place.jsx
@@ -0,0 +1,9 @@
+export class Place {
+    constructor(title, imageUri, location, id, address) {
+      this.title = title;
+      this.imageUri = imageUri;
+      this.address = address;
+      this.location =location; // { lat: 0.141241, lng: 127.121 }
+      this.id = new Date().toString() + Math.random().toString();
+    }
+  }
