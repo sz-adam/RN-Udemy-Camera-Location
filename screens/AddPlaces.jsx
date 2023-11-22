f35652c1dcb3301dbad2d82538df32ef82d878f6
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-22 17:59:00 +0100

    add screens

---
+++ screens/AddPlaces.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AddPlaces = () => {
+  return (
+    <View>
+      <Text>AddPlaces</Text>
+    </View>
+  )
+}
+
+export default AddPlaces
+
+const styles = StyleSheet.create({})
