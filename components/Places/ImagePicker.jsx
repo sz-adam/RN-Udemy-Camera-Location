5784a7021b566bbd2991c89b66d690b8694f40cd
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-22 19:35:54 +0100

    open camera

--- components/Places/ImagePicker.jsx
+++ components/Places/ImagePicker.jsx
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { launchCameraAsync, useCameraPermissions } from 'expo-image-picker'
+import { Colors } from '../../constans/colors';
 
 const ImagePicker = () => {
-  return (
-    <View>
-      <Text>ImagePicker</Text>
-    </View>
-  )
+    async function takeImageHandler() {
+        const image = await launchCameraAsync({
+            allowsEditing: true,
+            aspect: [16, 9],
+            quality: 0.5,
+        });
+        console.log(image)
+    }
+    return (
+        <View>
+          <View></View>
+        <Button title="Take Image " onPress={takeImageHandler} />
+        </View>
+    );
 }
 
-export default ImagePicker
+export default ImagePicker;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    imagePreview: {
+        width: '100%',
+        height: 200,
+        marginVertical: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.primary100,
+        borderRadius: 4,
+        overflow: 'hidden',
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+});
--- components/Places/PlaceForm.jsx
+++ components/Places/PlaceForm.jsx
@@ -1,6 +1,7 @@
 import { ScrollView, StyleSheet, Text, View, TextInput } from 'react-native'
 import React, { useState } from 'react'
 import { Colors } from '../../constans/colors';
+import ImagePicker from './ImagePicker';
 
 
 const PlaceForm = () => {
@@ -15,6 +16,7 @@ const PlaceForm = () => {
         <Text style={styles.label}> Title </Text>
         <TextInput style={styles.input} onChangeText={changetTitleHandler} value={enteredTitle} />
       </View>
+      <ImagePicker />
     </ScrollView>
   )
 }
