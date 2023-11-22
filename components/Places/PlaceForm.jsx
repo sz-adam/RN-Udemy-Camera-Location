71705970b55176d1741f6bba1763267eed0014ab
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-22 19:01:22 +0100

    style format  input

--- components/Places/PlaceForm.jsx
+++ components/Places/PlaceForm.jsx
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, Text, View, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import { Colors } from '../../constans/colors';
+
 
 const PlaceForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+
+  function changetTitleHandler(enteredText) {
+    setEnteredTitle(enteredText)
+  }
   return (
-    <View>
-      <Text>PlaceForm</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}> Title </Text>
+        <TextInput style={styles.input} onChangeText={changetTitleHandler} value={enteredTitle} />
+      </View>
+    </ScrollView>
   )
 }
 
 export default PlaceForm
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
