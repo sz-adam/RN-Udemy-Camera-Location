b9755c9fb591bbbbc6af3a9091d421f9449f1389
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-22 18:25:42 +0100

    add new place components

--- screens/AddPlaces.jsx
+++ screens/AddPlaces.jsx
@@ -1,12 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import PlaceForm from '../components/Places/PlaceForm'
 
 const AddPlaces = () => {
-  return (
-    <View>
-      <Text>AddPlaces</Text>
-    </View>
-  )
+  return <PlaceForm />
 }
 
 export default AddPlaces
